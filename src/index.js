484a4048028e9323deda753d815d0577aee65525
Surendra Kumar <surendrakr-94>
2022-10-28 09:30:47 +0530

    final soln

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://pritamsam1:Pritamsam1@project1.jichf9l.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Surendra-fn:BJCg2QysZDegXv22@cluster0.ktzzu7c.mongodb.net/PROJECT1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
